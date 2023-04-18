4ac3644be278add84c8d7eb88ae64d31d3891b09
DhruvNarola21 <131172732+DhruvNarola21@users.noreply.github.com>
2023-04-18 19:17:50 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float r,ans;
+	
+	printf("Enter radius:");
+	scanf("%f",&r);
+	
+	ans=3.14*r*r;
+	
+	printf("Area Of Circle Is:%f",ans);
+	
+	return 0;
+}
---
+++ task3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int A,B,ans;
+	
+	printf("Enter A:");
+	scanf("%d",&A);
+	
+	printf("Enter B:");
+	scanf("%d",&B);
+	
+	printf("Addition of A & B is:%d\n",A+B);
+	
+	printf("Subtraction of A & B is:%d\n",A-B);
+		
+	printf("Multiplication of A & B is:%d\n",A*B);
+			
+	printf("Division of A & B is:%d\n",A/B);
+				
+				
+	
+	return 0;
+}
