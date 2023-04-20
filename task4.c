6224f43db8c014f1df54bf10e0e775c8bb62052e
DhruvNarola21 <131172732+DhruvNarola21@users.noreply.github.com>
2023-04-20 08:39:34 +0530

    Add files via upload

---
+++ task4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int Width,height,Aor;
+	
+	printf("Enter Width:");
+	scanf("%d",&Width);
+	
+	printf("Enter height:");
+	scanf("%d",&height);
+	
+	Aor=Width*height;
+	
+	printf("Area Of Rectangle Is:%d",Aor);
+	
+	return 0;
+}
